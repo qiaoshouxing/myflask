3ca446d36a1894fd990edf33aa9d460be99e0142
qiaoshouxing <qiaoshouxingsdd@163.com>
2026-02-12 23:56:01 +0800

    add new product & footer

--- manhair/__init__.py
+++ manhair/__init__.py
@@ -15,6 +15,90 @@ def create_app(config_name = None):
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        products = [
+            {
+                'id': 1,
+                'name': 'Pleated skater skirt',
+                'link': '',
+                'images': ['image/product/p-1.jpg', 'image/product/p-2.jpg']
+            },
+            {
+                'id': 2,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-3.jpg', 'image/product/p-4.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 3,
+                'name': 'Girls floral ruffle top',
+                'link': '',
+                'images': ['image/product/p-5.jpg', 'image/product/p-6.jpg']
+            },
+            {
+                'id': 4,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-7.jpg', 'image/product/p-8.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 5,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-9.jpg', 'image/product/p-10.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 6,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-11.jpg', 'image/product/p-12.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 7,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-13.jpg', 'image/product/p-14.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 8,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-15.jpg', 'image/product/p-16.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 9,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-17.jpg', 'image/product/p-18.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 10,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-19.jpg', 'image/product/p-20.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 11,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-21.jpg', 'image/product/p-22.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            },
+            {
+                'id': 12,
+                'name': 'Tailored blazer jacket',
+                'link': '',
+                'images': ['image/product/p-23.jpg', 'image/product/p-24.jpg'],
+                'label': {'type': 'new', 'text': 'New', 'position': 'left'}
+            }
+        ]
+        return render_template('index.html', products=products)
 
     return app
